5937df91b17060e6651a8c6e78dd032db34ca58f
delvaleng <delgadogloribelv@gmail.com>
2021-08-07 18:45:52 -0500

    loisto

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,11 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .browserSync('http://bank-sys.test')
+    .disableNotifications();
 
-mix.disableNotifications();
-
-// mix.browserSync('http://bank-sys.test/');
 
 // mix.options({
 //   hmrOptions: {
